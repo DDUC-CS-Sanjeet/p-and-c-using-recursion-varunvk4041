e7b78ce77461b330a0e5d214f04bc8da8c0241fd
Varun <varunvk4041@gmail.com>
2020-01-24 17:08:43 +0530

    2nd commit

--- perm_and_comb.cpp
+++ perm_and_comb.cpp
@@ -1,17 +1,49 @@
 #include<iostream>
 using namespace std;
-
 int permutation(int n, int r)
 {
-  return 0;
+  if( r==0 )
+  {
+  	return 1;
+  }
+  else
+  {							
+	return n*permutation(n-1,r-1);
+  } 
 }
 
 int combination(int n, int r)
 {
-  return 0;
+  if( r==0 )
+  {
+  	return 1;
+  }
+  else
+  {
+  	return (n*1.0/r)*combination( n-1,r-1 );		
+  }
 }
 
 int main()
 {
+  int n,r;
+  cout<<"Enter values of N and R: ";
+  cin>>n>>r;
+  try
+  {
+  	if( n<0 || r<0 || n<r )
+  	{
+  		throw "Invalid Arguements";
+	}
+	else
+	{
+		cout<<"\nPermutation ="<<permutation( n,r );
+		cout<<"\nCombination ="<<combination( n,r );
+	}
+  }
+  catch( const char* s)
+  {
+  	cout<<"\n"<<s;
+  }
   return 0;
 }
